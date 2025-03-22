035ec53c67177f87a01b8ec64cc88d9dfc06322a
Harry EMARH <emarh.harry.code@gmail.com>
2025-03-22 23:23:55 +0100

    Auth Service 2

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { User, UserDocument } from 'src/user/user.schema';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    @InjectModel(User.name) private userModel: Model<UserDocument>,
+    private jwtService: JwtService,
+  ) {}
+}
