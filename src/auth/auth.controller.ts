29abb077d4cb3fe0d5da6f72800eba29b9e9ef0f
Harry EMARH <emarh.harry.code@gmail.com>
2025-03-23 00:00:30 +0100

    Auth Controller 3

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,30 +1,23 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UsePipes, ValidationPipe } from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { RegisterDto } from './dto/register.dto';
+import { LoginDto } from './dto/login.dto';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('register')
-  register(
-    @Body()
-    body: {
-      firstName: string;
-      lastName: string;
-      email: string;
-      password: string;
-    },
-  ) {
-    return this.authService.register(
-      body.firstName,
-      body.lastName,
-      body.email,
-      body.password,
-    );
+  @UsePipes(new ValidationPipe({ whitelist: true }))
+  register(@Body() registerDto: RegisterDto) {
+    const { firstName, lastName, email, password } = registerDto;
+    return this.authService.register(firstName, lastName, email, password);
   }
 
   @Post('login')
-  login(@Body() body: { email: string; password: string }) {
-    return this.authService.login(body.email, body.password);
+  @UsePipes(new ValidationPipe({ whitelist: true }))
+  login(@Body() loginDto: LoginDto) {
+    const { email, password } = loginDto;
+    return this.authService.login(email, password);
   }
 }
