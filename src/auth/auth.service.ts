1b223f359b4cd58ab5195c2c8d43c9ffc3b3775a
Harry EMARH <emarh.harry.code@gmail.com>
2025-03-22 23:42:31 +0100

    Generate Token

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,6 +3,7 @@ import { JwtService } from '@nestjs/jwt';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { User, UserDocument } from 'src/user/user.schema';
+import * as bcrypt from 'bcryptjs';
 
 @Injectable()
 export class AuthService {
@@ -10,4 +11,13 @@ export class AuthService {
     @InjectModel(User.name) private userModel: Model<UserDocument>,
     private jwtService: JwtService,
   ) {}
+
+  async register(firstName: string, lastName: string, email: string, password: string) {
+    // const userExist = await bcrypt
+  }
+
+  generateToken(user: UserDocument) {
+    const payload = { sub: user._id, email: user.email, role: user.role };
+    return { access_token: this.jwtService.sign(payload) };
+  }  
 }
