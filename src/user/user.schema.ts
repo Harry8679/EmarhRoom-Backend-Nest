9da80b63eef2c94b14a925fdaccc58eec8014486
Harry EMARH <emarh.harry.code@gmail.com>
2025-03-22 22:32:58 +0100

    User Schema 3

--- src/user/user.schema.ts
+++ src/user/user.schema.ts
@@ -5,6 +5,12 @@ export type UserDocument = User & Document;
 
 @Schema()
 export class User {
+  @Prop({ required: true })
+  firstName: string;
+
+  @Prop({ required: true })
+  lastName: string;
+
   @Prop({ required: true, unique: true })
   email: string;
 
