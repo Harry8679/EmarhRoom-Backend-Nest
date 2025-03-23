5d3ce7c7cdc48d9e496c96a964995be8fb79fd7f
Harry EMARH <emarh.harry.code@gmail.com>
2025-03-23 04:34:19 +0100

    App Module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,26 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
+    // Charge .env globalement
     ConfigModule.forRoot({ isGlobal: true }),
-    AuthModule, // ✅ Ajoute bien ça
+
+    // Connexion MongoDB via la variable d'environnement
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGO_URI'),
+      }),
+      inject: [ConfigService],
+    }),
+
+    // Tes autres modules
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
